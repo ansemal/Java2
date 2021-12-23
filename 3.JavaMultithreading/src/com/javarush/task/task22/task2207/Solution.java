d23e9f6b470c8debc275f310d7198fa3c9537ecc
Sergei <ansemal@yandex.ru>
2021-12-23 12:42:59 +0200

    Task 2207 (Palindrome)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2207/Solution.java
@@ -0,0 +1,96 @@
+package com.javarush.task.task22.task2207;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/* 
+Обращенные слова
+*/
+
+public class Solution {
+    public static List<Pair> result = new LinkedList<>();
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             BufferedReader fileReader = new BufferedReader(new FileReader(reader.readLine()))) {
+            while (fileReader.ready()) {
+                Collections.addAll(list, fileReader.readLine().split(" "));
+            }
+            for (int i = 0; i < list.size(); i++) {
+                for (int j = 0; j < list.size(); ) {
+                    if (i >= list.size()) {
+                        break;
+                    }
+                    if (list.get(j).equals(new StringBuilder(list.get(i)).reverse().toString()) && j != i) {
+                        Pair pair = new Pair();
+                        pair.first = list.get(j);
+                        pair.second = list.get(i);
+                        result.add(pair);
+                        list.remove(j);
+                        list.remove(i);
+                        j = 0;
+                    } else
+                        j++;
+                }
+            }
+            result.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static class Pair {
+        String first;
+        String second;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Pair pair = (Pair) o;
+            if (first != null ? !first.equals(pair.first) : pair.first != null) return false;
+            return second != null ? second.equals(pair.second) : pair.second == null;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = first != null ? first.hashCode() : 0;
+            result = 31 * result + (second != null ? second.hashCode() : 0);
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return first == null && second == null ? "" :
+                    first == null ? second :
+                            second == null ? first :
+                                    first.compareTo(second) < 0 ? first + " " + second : second + " " + first;
+        }
+    }
+}
+/*
+В методе main с консоли считать имя файла, который содержит слова, разделенные пробелами.
+Найти в тексте все пары слов, которые являются обращением друг друга. Добавить их в result.
+Использовать StringBuilder.
+Кодировка файла - UTF-8.
+
+Пример содержимого файла
+рот тор торт о
+о тот тот тот
+
+Вывод:
+рот тор
+о о
+тот тот
+
+Requirements:
+1. Метод main должен считывать имя файла с клавиатуры.
+2. В методе main должен быть использован StringBuilder.
+3. В классе Solution должен содержаться вложенный класс Pair с методами equals, hashCode и toString. Удалять или изменять эти методы нельзя.
+4. В классе Pair должен быть объявлен конструктор без параметров (или конструктор по умолчанию).
+5. Список result должен быть заполнен корректными парами согласно условию задачи.
+ */
