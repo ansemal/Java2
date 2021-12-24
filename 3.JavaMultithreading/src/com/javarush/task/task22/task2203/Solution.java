0ddeec66a2cc6f0f753a9c1868ea5a1a20e26dc7
Sergei <ansemal@yandex.ru>
2021-12-24 12:03:12 +0200

    Task 2203 (between tabs )

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2203/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task22.task2203;
+
+/* 
+Между табуляциями
+*/
+
+public class Solution {
+    public static String getPartOfString(String string) throws TooShortStringException {
+        if (string==null || string.length()<2)
+            throw new TooShortStringException();
+        int firstIndex = string.indexOf('\t');
+        int secondIndex = string.indexOf('\t', firstIndex+1);
+        if (firstIndex<0 || secondIndex<0)
+            throw new TooShortStringException();
+        return string.substring(firstIndex+1, secondIndex);
+    }
+
+    public static class TooShortStringException extends Exception {
+    }
+
+    public static void main(String[] args) throws TooShortStringException {
+        System.out.println(getPartOfString("\tJavaRush - лучший сервис \tобучения Java\t."));
+    }
+}
+
+/*
+Метод getPartOfString должен возвращать подстроку между первой и второй табуляцией.
+На некорректные данные бросить исключение TooShortStringException.
+Класс TooShortStringException не менять.
+Requirements:
+1. Класс TooShortStringException должен быть потомком класса Exception.
+2. Метод getPartOfString должен принимать строку в качестве параметра.
+3. В случае, если строка, переданная в метод getPartOfString содержит менее 2 табуляций должно возникнуть исключение TooShortStringException.
+4. Метод getPartOfString должен возвращать подстроку между первой и второй табуляцией.
+ */
